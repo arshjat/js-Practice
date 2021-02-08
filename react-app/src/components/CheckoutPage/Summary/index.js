5c53a090b28463158e6462b5c00538546e4e161e
Arsh Panghal <panghalarsh@gmail.com>
2021-02-08 23:34:43 +0530

    fixed Summary component

--- react-app/src/components/CheckoutPage/Summary/index.js
+++ react-app/src/components/CheckoutPage/Summary/index.js
@@ -4,8 +4,6 @@ export default function Summary({bagTotal}){
     return (
             <div className="summary-details">
                  <div className="prices-summary">
-                   <div className="container">
-                       
                         <div className="prices-header">
                             <div>Price Details</div>
                         </div>
@@ -14,7 +12,7 @@ export default function Summary({bagTotal}){
                             <div>Bag Total</div>
                             <div>₹{bagTotal}</div>
                         </div>
-                        {/* 
+                        
                         <div className="price-rows">
                             <div>Bag Discount</div>
                             <div>₹0</div>
@@ -23,25 +21,22 @@ export default function Summary({bagTotal}){
                             <div>Tax</div>
                             <div>₹0</div>
                         </div>
-                        */}
+                        
                         
                         <div className="price-rows">
                             <div>Coupon Discount</div>
-                            <button style={{'background':'transparent','color':'blue','border':'none'}} id="coupon-link">Apply</button>
+                            <button style={{'background':'transparent','color':'blue','border':'none','font-size':'1.8vmin'}} id="coupon-link">Apply</button>
                         </div>
-                        {/*
+                        
                         <div className="price-rows">
                             <div>Delivery Charges</div>
                             <div>₹0</div>
-                        </div> */}
+                        </div> 
                     </div> 
-                </div>      
                 <hr width="96%" />
                 <div className="total">
-                    <div className="totalPrice">
                         <div>Total</div>
                         <div>₹{bagTotal}</div>
-                    </div>
                 </div>
                 <div className="order-button">
                     <button id="order-button">Proceed To Pay</button>
